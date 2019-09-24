e14cd089094ed3f7160a119d8cbfe9619700cf1c
Iori Minami <iori.minami@gmail.com>
2019-09-24 13:45:07 +0900

    first commit

---
+++ techitter/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  techitter
+//
+//  Created by 南伊織 on 2018/09/15.
+//  Copyright © 2018年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
